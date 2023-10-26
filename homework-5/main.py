3d1477d601eaf7db8f838f7bf76130391c0c899d
ViktorDavydov <davydov.viktor.andreevich@gmail.com>
2023-10-26 21:06:14 +0300

    5th homework commit

--- homework-5/main.py
+++ homework-5/main.py
@@ -8,9 +8,11 @@ from config import config
 def main():
     script_file = 'fill_db.sql'
     json_file = 'suppliers.json'
-    db_name = 'my_new_db'
+    db_name = 'hw5_db'
+    ini_config_file = 'database.ini'
+    section_params = 'postgresql'
 
-    params = config()
+    params = config(ini_config_file, section_params)
     conn = None
 
     create_database(params, db_name)
@@ -18,7 +20,8 @@ def main():
 
     params.update({'dbname': db_name})
     try:
-        with psycopg2.connect(**params) as conn:
+        connection = psycopg2.connect(**params)
+        with connection as conn:
             with conn.cursor() as cur:
                 execute_sql_script(cur, script_file)
                 print(f"БД {db_name} успешно заполнена")
@@ -42,32 +45,57 @@ def main():
 
 def create_database(params, db_name) -> None:
     """Создает новую базу данных."""
-    pass
+    try:
+        connection = psycopg2.connect(host=params['host'], user=params['user'],
+                                      password=params['password'], port=params['port'])
+        connection.autocommit = True
+        cur = connection.cursor()
+        cur.execute(f"DROP DATABASE IF EXISTS {db_name};")
+        cur.execute(f"CREATE DATABASE {db_name};")
 
-def execute_sql_script(cur, script_file) -> None:
+    finally:
+        connection.close()
+
+
+def execute_sql_script(cursor, fill_script_file) -> None:
     """Выполняет скрипт из файла для заполнения БД данными."""
+    with open(fill_script_file, 'r') as file:
+        cursor.execute(file.read())
 
 
-
-def create_suppliers_table(cur) -> None:
+def create_suppliers_table(cursor) -> None:
     """Создает таблицу suppliers."""
-    pass
+    cursor.execute("DROP TABLE IF EXISTS suppliers;")
+    cursor.execute("CREATE TABLE suppliers"
+                   "("
+                   "    company_name varchar(50),"
+                   "    contact varchar(100),"
+                   "    address text,"
+                   "    phone varchar(25),"
+                   "    fax varchar(25),"
+                   "    homepage text,"
+                   "    products text"
+                   ")")
 
 
 def get_suppliers_data(json_file: str) -> list[dict]:
     """Извлекает данные о поставщиках из JSON-файла и возвращает список словарей с соответствующей информацией."""
-    pass
+    with open(json_file, 'r', encoding='UTF-8') as source_file:
+        data = json.load(source_file)
+        return data
 
 
-def insert_suppliers_data(cur, suppliers: list[dict]) -> None:
+def insert_suppliers_data(cursor, suppliers: list[dict]) -> None:
     """Добавляет данные из suppliers в таблицу suppliers."""
-    pass
+    for item in suppliers:
+        cursor.execute(f"INSERT INTO suppliers VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                       (item['company_name'], item['contact'], item['address'],
+                        item['phone'], item['fax'], item['homepage'], ", ".join(item['products'])))
 
 
-def add_foreign_keys(cur, json_file) -> None:
+def add_foreign_keys(cursor, json_file) -> None:
     """Добавляет foreign key со ссылкой на supplier_id в таблицу products."""
     pass
-
 
 if __name__ == '__main__':
     main()
